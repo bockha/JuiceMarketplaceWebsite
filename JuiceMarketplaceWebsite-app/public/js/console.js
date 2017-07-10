17076285eabd8f979701a609763ae4f817896a34
liefersfl <florian@liefers.com>
2017-07-10 14:36:41 +0200

    initial commit of tdm console files.

---
+++ JuiceMarketplaceWebsite-app/public/js/console.js
@@ -0,0 +1,30 @@
+		/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+		function toggleNav() {
+            if ($("#sidebar").hasClass("open")) {
+                closeNav();
+            } else {
+                openNav();
+            }
+		}
+
+		function openNav() {
+            $("#sidebar").addClass("open");
+			document.getElementById("sidebar").style.width = "250px";
+			document.getElementById("main").style.marginLeft = "250px";
+		}
+
+		/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+		function closeNav() {
+            $("#sidebar").removeClass("open");
+			document.getElementById("sidebar").style.width = "0";
+			document.getElementById("main").style.marginLeft = "0";
+		}
+
+        function loadConfigurator() {
+            $('body > div[role="dialog"]').remove();
+            $("#page_content").load("configurator.html");
+        }
+
+		$( function() {
+            openNav();
+        })
