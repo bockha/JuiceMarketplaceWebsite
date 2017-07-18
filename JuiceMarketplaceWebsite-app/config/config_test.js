4cc68cd1eb95424032070c4c89fce63e29f00403
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-07-18 17:16:51 +0200

    Updated test config

---
+++ JuiceMarketplaceWebsite-app/config/config_test.js
@@ -0,0 +1,51 @@
+/**
+ * Created by beuttlerma on 18.04.17.
+ */
+
+
+var self = {};
+
+
+// ---- CONFIGURATION EXPORT ----
+self.LOG_LEVEL = 'debug';
+
+self.HOST_SETTINGS = {
+    MARKETPLACE_CORE: {
+        HOST: 'test-tdm.fritz.box',
+        PORT: 3002
+    },
+    OAUTH_SERVER: {
+        HOST: 'test-tdm.fritz.box',
+        PORT: 3006
+    }
+};
+
+self.OAUTH_PROVIDER = {
+    facebookAuth: {
+        'clientID': '', // your App ID
+        'clientSecret': '', // your App Secret
+        'callbackURL': 'http://localhost:8080/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields': ['id', 'name', 'photos']
+    },
+
+    twitterAuth: {
+        'consumerKey': '',
+        'consumerSecret': '',
+        'callbackURL': 'http://127.0.0.1:3004/auth/twitter/callback'
+    },
+
+    googleAuth: {
+        'clientID': '',
+        'clientSecret': '',
+        'callbackURL': 'http://127.0.0.1:3004/auth/google/callback'
+    }
+};
+
+self.OAUTH_CREDENTIALS = {
+    CLIENT_ID: '43d9bfb5-cdf4-4a00-9658-175397a69efc',
+    CLIENT_SECRET: 'is secret ;-)'
+};
+
+
+module.exports = self;
