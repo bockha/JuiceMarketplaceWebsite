d8df3e1402a8d91b889793d4561656e36f3977f7
TRUMPF\ElyGomesMa <marcel.elygomes@de.trumpf.com>
2017-08-03 12:39:04 +0200

    Reports for user

---
+++ JuiceMarketplaceWebsite-app/public/myreports/app/services/myreports-data-service.js
@@ -0,0 +1,111 @@
+angular.module('myreports').factory('MyReportsDataService', ['$q', '$http', 'moment', function($q, $http, moment) {
+    
+    function getDrinksByHours(hours) {
+        var defer = $q.defer();
+        var sinceDate = moment().subtract(hours, 'hours').format('YYYY-MM-DD HH:mm:ss');
+        $http({
+            method: 'GET',
+            url: '/myreports?sinceDate=' + sinceDate
+        }).then(function(result) {
+            defer.resolve(result);
+        }, function(error) {
+            defer.reject(error);
+        });
+        return defer.promise;
+    }
+
+
+    function getTopDrinksEver() {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: '/myreports?sinceDate=' + moment('1970-01-01').format('YYYY-MM-DD HH:mm:ss') + '&topValue=10'
+        }).then(function(result) {
+            defer.resolve(result);
+        }, function(error) {
+            defer.reject(error);
+        });
+        return defer.promise;    
+    }
+
+
+    function getTopDrinksOfToday() {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: '/myreports?sinceDate=' +  moment().subtract(24, 'hours').format('YYYY-MM-DD HH:mm:ss') + '&topValue=10'
+        }).then(function(result) {
+            defer.resolve(result);
+        }, function(error) {
+            defer.reject(error);
+        });
+        return defer.promise;    
+    }
+
+
+    function getFavoriteJuices() {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: '/myreports/favorit?sinceDate=' +  moment('1970-01-01').format('YYYY-MM-DD HH:mm:ss') + '&topValue=10'
+        }).then(function(result) {
+            defer.resolve(result);
+        }, function(error) {
+            defer.reject(error);
+        });
+        return defer.promise;
+    }
+
+
+    function getWorkload() {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: '/reports/workload?sinceDate=' +  moment().startOf('day').format('YYYY-MM-DD HH:mm:ss')
+        }).then(function(result) {
+            defer.resolve(result);
+        }, function(error) {
+            defer.reject(error);
+        });
+        return defer.promise;
+    }
+
+
+    function getRevenuePerHour() {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: '/myreports/revenue?sinceDate=' +  moment().startOf('day').format('YYYY-MM-DD HH:mm:ss') + '&time=hour'
+        }).then(function(result) {
+            defer.resolve(result);
+        }, function(error) {
+            defer.reject(error);
+        });
+        return defer.promise;
+    }
+
+
+    function getRevenuePerDay() {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: '/myreports/revenue?sinceDate=' +  moment().subtract(700, 'days').format('YYYY-MM-DD HH:mm:ss') + '&time=day'
+        }).then(function(result) {
+            defer.resolve(result);
+        }, function(error) {
+            defer.reject(error);
+        });
+        return defer.promise;
+    }
+
+
+    return {
+        getDrinksByHours: getDrinksByHours,
+        getTopDrinksEver: getTopDrinksEver,
+        getTopDrinksOfToday: getTopDrinksOfToday,
+        getFavoriteJuicesSince: getFavoriteJuices,
+        getWorkloadSince: getWorkload,
+        getRevenuePerHour: getRevenuePerHour,
+        getRevenuePerDay: getRevenuePerDay
+    };
+}]);
---
+++ JuiceMarketplaceWebsite-app/routes/myreports.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+var marketplaceCore = require('../adapter/marketplace_core_adapter');
+
+router.get('/', function (req, res, next) {
+
+    var token = req.user.token;
+    marketplaceCore.getTopDrinksSinceForUser(token, req.query['sinceDate'], req.query['topValue'], function (err, data) {
+        if (err) {
+            res.status(500);
+            res.send('Error when requesting data from the marketplace core');
+
+            return;
+        }
+
+        res.json(data);
+    })
+});
+
+router.get('/favorit', function (req, res, next) {
+
+    var token = req.user.token;
+    marketplaceCore.getFavoriteJuicesSinceForUser(token, req.query['sinceDate'], function (err, data) {
+        if (err) {
+            res.status(500);
+            res.send('Error when requesting data from the marketplace core');
+
+            return;
+        }
+
+        res.json(data);
+    })
+});
+
+router.get('/workload', function (req, res, next) {
+
+    var token = req.user.token;
+    marketplaceCore.getWorkloadSinceForUser(token, req.query['sinceDate'], function (err, data) {
+        if (err) {
+            res.status(500);
+            res.send('Error when requesting data from the marketplace core');
+
+            return;
+        }
+
+        res.json(data);
+    })
+});
+
+router.get('/revenue', function (req, res, next) {
+
+    var token = req.user.token;
+    marketplaceCore.getRevenueForUser(token, req.query['sinceDate'], req.query['time'], function (err, data) {
+        if (err) {
+            res.status(500);
+            res.send('Error when requesting data from the marketplace core');
+
+            return;
+        }
+
+        res.json(data);
+    })
+});
+
+module.exports = router;
