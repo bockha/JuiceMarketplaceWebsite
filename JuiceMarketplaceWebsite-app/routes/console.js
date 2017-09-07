6fd8594d7b8cee72dd0e862623d57c3cb1f747e3
liefersfl <florian@liefers.com>
2017-09-07 10:26:26 +0200

    made RECIPE_LIMIT_PER_USER working again, error message is now with handled through ejs

--- JuiceMarketplaceWebsite-app/routes/console.js
+++ JuiceMarketplaceWebsite-app/routes/console.js
@@ -1,5 +1,7 @@
+const marketplaceCore = require('../adapter/marketplace_core_adapter');
 const express = require('express');
 const router = express.Router();
+const CONFIG = require('../config/config_loader');
 
 router.get('/', function (req, res, next) {
     res.redirect('myreports');
@@ -9,8 +11,23 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/configurator', function (req, res, next) {    
-    res.render('console/configurator', {
-        query: req.query,
+    // Check if user can still publish recipes or if his limit is reached.
+    marketplaceCore.getRecipesForUser(req.user.token.user, req.user.token.accessToken, function (err, recipes) {
+        if (err) {
+            return next(err);
+        }
+        if (recipes.length >= CONFIG.RECIPE_LIMIT_PER_USER) {
+            console.log("Max number of recipes reached. Redirecting to myrecipes.")
+            res.render('console/myrecipes', {
+                query: req.query,
+                maxRecipesError: true,
+            });
+            // return res.redirect('/console/myrecipes');
+        } else {
+            res.render('console/configurator', {
+                query: req.query,
+            });
+        }
     });
 });
 
