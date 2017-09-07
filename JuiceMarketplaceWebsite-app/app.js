64d46928ad4b7632373354ad21979ccffdcbd5c8
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-09-07 16:29:26 +0200

    do not logout user on failure

--- JuiceMarketplaceWebsite-app/app.js
+++ JuiceMarketplaceWebsite-app/app.js
@@ -99,8 +99,9 @@ app.use(function (req, res, next) {
 
 if (app.get('env') !== 'development') {
     app.use(function(err, req, res, next) {
-        //Always logout user on failure
-        req.logout();
+        if (err.status === 401) {
+            req.logout();
+        }
         next(err, req, res)
     });
 }
