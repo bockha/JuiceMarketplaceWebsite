eb069fc5f8175251606f653347914742fc09d5a8
liefersfl <florian@liefers.com>
2017-09-11 19:03:41 +0200

    removed unnecessary code

--- JuiceMarketplaceWebsite-app/public/console/js/console.js
+++ JuiceMarketplaceWebsite-app/public/console/js/console.js
@@ -1,32 +1,3 @@
-var consolePages = {
-    configurator: {
-        title: "Neues Getränk anlegen",
-        url: "configurator.html"
-    },
-    dashboard: {
-        title: "Dashboard",
-        url: "myreports.html"
-    },
-    myrecipes: {
-        title: "Getränke verwalten",
-        url: "myrecipes.html"
-    }
-};
-
-function getUrlParameter(name) {
-    var value = null;
-    var query = window.location.search.substring(1);
-    var parametersAndValues = query.split("&");
-    for (var i = 0; i < parametersAndValues.length; i += 1) {
-        var split = parametersAndValues[i].split("=");
-        if (split[0] == name) {
-            value = split[1];
-            break;
-        }
-    }
-    return value;
-}
-
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function toggleNav() {
     if ($("#sidebar").hasClass("open")) {
@@ -49,45 +20,6 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-function loadConfigurator() {
-    window.location.href = "console.html?configurator";
-    // $('body > div[role="dialog"]').remove();
-    // $("#page_content").load("configurator.html");
-}
-
-function getDisplayName(userName, firstName, lastName) {
-    if (firstName && lastName) {
-        return firstName + " " + lastName;
-    }
-
-    if (userName) {
-        return userName;
-    }
-
-    if (firstName) {
-        return firstName
-    }
-
-    if (lastName) {
-        return lastName
-    }
-
-    return 'Anonymous';
-}
-
 $(function () {
     openNav();
 });
-
-function checkRecipeLimit() {
-    $.getJSON('/users/me/recipes/limit', function(data) {
-        if (data && data.limit > 0) {
-            window.location.href = "/console/configurator";
-        }
-        else {
-            window.location.href = "/console/myrecipes&limit_reached=true";
-        }
-    }).fail(function() {
-        window.location.href = "/console/myrecipes";
-    })
-}
