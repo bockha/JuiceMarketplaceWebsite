d563a1b8d3bdd00a2aa39768c6a244c87847ee0c
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-09-18 10:07:30 +0200

    Added hourly session clean up interval to remove expired session objects

--- JuiceMarketplaceWebsite-app/oauth/passport.js
+++ JuiceMarketplaceWebsite-app/oauth/passport.js
@@ -1,10 +1,37 @@
 // load all the things we need
-var fs = require('fs');
-var path = require('path');
-var logger = require('../global/logger');
-var authServer = require('../adapter/auth_service_adapter');
+const fs = require('fs');
+const path = require('path');
+const logger = require('../global/logger');
+const authServer = require('../adapter/auth_service_adapter');
 
-var userStore = {}; //TODO: Maybe move this into a database
+const userStore = {}; //TODO: Maybe move this into a database
+
+// Cleanup Session objects periodically
+const cleanUpInterval = setInterval(function() {
+    logger.info('[passport] Session cleanup');
+    Object.keys(userStore).forEach(function(uuid) {
+        try {
+            // Delete all sessions where refresh token has expired.
+            const session = userStore[uuid];
+            if (session) {
+                if (!session.token || !session.token.refreshTokenExpiresAt) {
+                    logger.info('[passport] deleting invalid session object for user: ' + uuid);
+                    delete userStore[uuid];
+
+                    return;
+                }
+
+                if (new Date(session.token.refreshTokenExpiresAt) < new Date()) {
+                    logger.info('[passport] refresh token expired. deleting session for user: ' + uuid);
+                    delete userStore[uuid];
+                }
+            }
+        }
+        catch (err) {
+            logger.warn(err);
+        }
+    });
+}, 1000 * 60 * 60);
 
 function configurePassportForStragiesInPath(dirPath, passport) {
     // Loop through all the files in the temp directory
@@ -60,8 +87,9 @@ module.exports = function (passport) {
 
         authServer.refreshTokenForUser(user, function (err, updatedUser) {
             if (err) {
-                logger.warn('Error while refreshing access token: ' + JSON.stringify(err));
-                logger.info('Forcing log out user.');
+                logger.warn('[passport] Error while refreshing access token');
+                logger.warn(err);
+                logger.info('[passport] Forcing log out user.');
 
                 done(null, false);
             }
