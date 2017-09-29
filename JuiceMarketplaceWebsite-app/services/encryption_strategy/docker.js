3f252b035e03e841be2012c7ecc5fd69474b9702
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-09-29 13:12:39 +0200

    Refactored encryption service to use different strategies depending on the node_env. In case of NODE_ENV=docker nothing will be encrypted

---
+++ JuiceMarketplaceWebsite-app/services/encryption_strategy/docker.js
@@ -0,0 +1,10 @@
+var self = require('./default');
+
+logger.warn('[encryption_service] RUNNING DOCKER (SIMULATION) MODE. Data will not being encrypted!');
+
+self.encryptData = function(data) {
+    return data;
+};
+
+module.exports = self;
+
