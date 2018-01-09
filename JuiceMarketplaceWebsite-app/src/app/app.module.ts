903ccd5f7730f890bc8a2e10b29767b5b39ae4fa
Christian Görg <christian.goerg@de.trumpf.com>
2018-01-09 07:00:02 +0100

    integrated cookieconsent lib for cookie information popup

--- JuiceMarketplaceWebsite-app/src/app/app.module.ts
+++ JuiceMarketplaceWebsite-app/src/app/app.module.ts
@@ -18,7 +18,36 @@ import {AccountComponent} from './account/account.component';
 import {ConsoleModule} from './console/console.module';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {NgcCookieConsentConfig, NgcCookieConsentModule} from 'ngx-cookieconsent';
 
+
+const cookieConfig: NgcCookieConsentConfig = {
+    "cookie": {
+        "domain": "localhost"
+    },
+    "position": "bottom-right",
+    "theme": "classic",
+    "palette": {
+        "popup": {
+            "background": "#434343",
+            "text": "#ffffff",
+            "link": "#ffffff"
+        },
+        "button": {
+            "background": "#bdcc36",
+            "text": "#ffffff",
+            "border": "transparent"
+        }
+    },
+    "type": "info",
+    "content": {
+        "message": "Um unsere Webseite für Sie optimal gestalten zu können, verwenden wir Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.",
+        "dismiss": "Verstanden",
+        "deny": "Refuse cookies",
+        "link": "Mehr zum Thema Cookies",
+        "href": "https://cookiesandyou.com"
+    }
+};
 
 @NgModule({
     declarations: [
@@ -27,6 +56,7 @@ import {AppComponent} from './app.component';
         AccountComponent
     ],
     imports: [
+        NgcCookieConsentModule.forRoot(cookieConfig),
         CommonModule,
         BrowserAnimationsModule,
         AppRoutingModule,
@@ -38,6 +68,7 @@ import {AppComponent} from './app.component';
         MatMenuModule,
         FlexLayoutModule,
         ConsoleModule,
+
     ],
     providers: [],
     bootstrap: [AppComponent]
