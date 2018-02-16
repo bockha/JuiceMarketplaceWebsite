2280cc0f0f16da78c1b9b9733c3ccbfd994c5bba
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-02-16 06:35:54 +0100

    code reformatting

--- JuiceMarketplaceWebsite-app/src/app/console/vault-testnet-help-dialog/vault-testnet-help-dialog.component.ts
+++ JuiceMarketplaceWebsite-app/src/app/console/vault-testnet-help-dialog/vault-testnet-help-dialog.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialogRef} from "@angular/material";
 
 @Component({
-  selector: 'app-vault-testnet-help-dialog',
-  templateUrl: './vault-testnet-help-dialog.component.html',
-  styleUrls: ['./vault-testnet-help-dialog.component.css']
+    selector: 'app-vault-testnet-help-dialog',
+    templateUrl: './vault-testnet-help-dialog.component.html',
+    styleUrls: ['./vault-testnet-help-dialog.component.css']
 })
 export class VaultTestnetHelpDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<VaultTestnetHelpDialogComponent>) { }
+    constructor(public dialogRef: MatDialogRef<VaultTestnetHelpDialogComponent>) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
